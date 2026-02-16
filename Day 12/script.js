fe9c8ba2f27b07dc4e1eff5a0ba8be341ba2cd5c
Muhammad Ammar Jalal <iammar0018@gmail.com>
2026-02-16 14:08:55 +0500

    Js Voting Eligibility Check

---
+++ Day 12/script.js
@@ -0,0 +1,10 @@
+let age = prompt("Please enter your age:");
+
+age = Number(age);
+ 
+if (age >= 18) {
+    alert("You are eligible to vote.");
+} else {
+    alert("You are not eligible to vote.");
+}
+//This is a comment in javascript..
